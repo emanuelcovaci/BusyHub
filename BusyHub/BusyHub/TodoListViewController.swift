19ad5b1a064f46ca4936bf4bf9a06d8be29e6d21
Emanuel Covaci <covaciemanuel98@gmail.com>
2019-03-08 13:16:05 +0200

    Subclassing UITableViewController

---
+++ BusyHub/BusyHub/TodoListViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  BusyHub
+//
+//  Created by Emanuel Covaci on 08/03/2019.
+//  Copyright © 2019 Emanuel Covaci. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+    
+    let items = ["Work", "School", "Jogging"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return items.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "ToDoItemCell")
+        
+        cell.textLabel?.text = items[indexPath.row]
+        
+        return cell
+    }
+
+
+}
+
