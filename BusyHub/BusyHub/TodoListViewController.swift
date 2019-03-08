54b91d889b4d7aa2cd48b0643aa68bc18549a716
Emanuel Covaci <covaciemanuel98@gmail.com>
2019-03-08 14:47:35 +0200

    Add new item functionality complete

--- BusyHub/BusyHub/TodoListViewController.swift
+++ BusyHub/BusyHub/TodoListViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
-    let items = ["Work", "School", "Jogging"]
+    var items = ["Work", "School", "Jogging"]
     override func viewDidLoad() {
         super.viewDidLoad()
        
@@ -37,15 +37,29 @@ class TodoListViewController: UITableViewController {
             tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
             
         }
-        
-        
-        
-        
-       
-        
         tableView.deselectRow(at: indexPath, animated: true)
     }
 
 
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add a new BusyHud item", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add item", style: .default) { (action) in
+            print(textField.text!)
+            self.items.append(textField.text!)
+            self.tableView.reloadData()
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create a new item"
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
 }
 
