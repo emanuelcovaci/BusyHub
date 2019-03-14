2f31e7db3a226752dea8425c1f24bc0584bfa2df
Emanuel Covaci <covaciemanuel98@gmail.com>
2019-03-15 00:40:04 +0200

    Category view controller

---
+++ BusyHub/BusyHub/Controllers/CategoryViewController.swift
@@ -0,0 +1,89 @@
+//
+//  CategoryViewController.swift
+//  BusyHub
+//
+//  Created by Emanuel Covaci on 14/03/2019.
+//  Copyright © 2019 Emanuel Covaci. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+
+class CategoryViewController: UITableViewController {
+    
+    var categoriesArray = [Category]()
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    
+        loadCategories()
+    }
+
+
+    @IBAction func addButtonPressed(_ sender: Any) {
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add a new category", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add category", style: .default) { (action) in
+            print(textField.text!)
+            let category = Category(context: self.context)
+            category.name = textField.text!
+            self.categoriesArray.append(category)
+            self.saveCategories()
+            
+            self.tableView.reloadData()
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create a new category"
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return categoriesArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "CategoryCell")
+        
+        cell.textLabel?.text = categoriesArray[indexPath.row].name
+        
+        
+        return cell
+    }
+    
+    
+    func saveCategories(){
+        
+        do{
+            try context.save()
+        }catch {
+            print ("Error saving contex! \(error)")
+        }
+    }
+    
+    func loadCategories(with request:NSFetchRequest<Category> = Category.fetchRequest()){
+        
+        do{
+            categoriesArray = try context.fetch(request)
+        }catch{
+            print("Error fetching categories \(error)")
+        }
+        
+        tableView.reloadData()
+        
+    }
+    
+    
+    
+
+}
